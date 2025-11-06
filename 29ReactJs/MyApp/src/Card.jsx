c2cad8e5bb6a0ce7328644025fa7a8eb52f19185
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2025-11-06 18:34:50 +0530

    Update: Correct wording in React component guidelines and replace image source in Card component

--- 29ReactJs/MyApp/src/Card.jsx
+++ 29ReactJs/MyApp/src/Card.jsx
@@ -1,7 +1,7 @@
 function Card() {
     return (
         <div>
-            <img src="https://imgs.search.brave.com/nOslBBIvtbGgMAhaZbWj_X8yz-xqWSAA9vlL2hW9rm0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9ibGFjay13aXJl/bGVzcy1oZWFkc2V0/LXN0dWRpby0zZC1y/ZW5kZXJfMjUyNjAw/LTkzMC5qcGc_c2Vt/dD1haXNfaHlicmlk/Jnc9NzQwJnE9ODA" alt="" width="300" />
+            <img src="https://imgs.search.brave.com/B_yAIlIHnfzK_wtsVraML1fnbCEJR58zK2K1HZitMuc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wNTAv/NDY5LzUwNi9zbWFs/bC9tb2Rlcm4td2ly/ZWxlc3MtaGVhZHBo/b25lcy13aXRoLWJs/YWNrLWVhcmN1cHMt/YW5kLWEtd2hpdGUt/aGVhZGJhbmQtcmVz/dC1vbi1hLXdoaXRl/LXRhYmxlLXBob3Rv/LmpwZWc" alt="" width="300" />
             <h2>Next Gen Headphone</h2>
             <p>Rs. 12000</p>
         </div>
