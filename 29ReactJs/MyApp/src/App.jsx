4bcd6063067c3ba61d9ea0d22dc445af48dee162
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2025-11-06 18:34:33 +0530

    Refactor: Clean up App component by removing unused code and comments, and display firstName variable

--- 29ReactJs/MyApp/src/App.jsx
+++ 29ReactJs/MyApp/src/App.jsx
@@ -5,32 +5,45 @@ import './App.css'
 import Navbar, {Home} from './Navbar'
 import Card from './Card'
 
+// If you importing something that doesn't exists 
+// then it will show error in console
 
+
+
+// function App() {
+  
+//   return (
+    
+//     <div>
+//       {/* <Navbar/> */}
+//       {/* <Home/> */}
+
+//       {/* All Products will show in the middle of the web page because
+//           of App.css and index.css 
+//       */}
+//       <Card/> 
+//       <Card/>
+//       <Card/>
+//       <Card/>
+//     </div>
+//   )
+// }
+
+
+
+
+
+
+// Whenever you declare a variable, declare it inside the component function
+// otherwise, maybe it will create an issue later
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const firstName = 'Lakshaya';
+  
   return (
-    
     <div>
-      {/* <Navbar/> */}
-      {/* <Home/> */}
-
-      {/* All Products will show in the middle of the web page because
-          of App.css and index.css 
-      */}
-      <Card/> 
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      {/* Use {} to show variables value on UI */}
+      {firstName}
     </div>
   )
 }
