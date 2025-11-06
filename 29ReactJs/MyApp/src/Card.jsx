4f181af4c8c40f9254e8ebd93f67e2757f819a8d
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2025-11-06 18:06:20 +0530

    Add: Create Navbar and Card components, update App component structure

---
+++ 29ReactJs/MyApp/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return (
+        <div>
+            <img src="https://imgs.search.brave.com/nOslBBIvtbGgMAhaZbWj_X8yz-xqWSAA9vlL2hW9rm0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9ibGFjay13aXJl/bGVzcy1oZWFkc2V0/LXN0dWRpby0zZC1y/ZW5kZXJfMjUyNjAw/LTkzMC5qcGc_c2Vt/dD1haXNfaHlicmlk/Jnc9NzQwJnE9ODA" alt="" width="300" />
+            <h2>Next Gen Headphone</h2>
+            <p>Rs. 12000</p>
+        </div>
+    )
+}
+
+export default Card;
---
+++ 29ReactJs/MyApp/src/Navbar.jsx
@@ -0,0 +1,20 @@
+function Navbar() {
+    return (
+        <div>
+            Nav-bar
+        </div>
+    )
+}
+
+// Named export home component
+export function Home() {
+    return (
+        <div>
+            Home-Page
+        </div>
+    )
+}
+
+
+// Default export Navbar
+export default Navbar;
