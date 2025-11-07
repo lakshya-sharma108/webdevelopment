5bfd5033afbf3afe39d19fa81e18bc025ff57bb3
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2025-11-07 19:10:58 +0530

    Refactor: Update App and Card components to utilize props for dynamic data rendering

--- 29ReactJs/MyApp/src/App.jsx
+++ 29ReactJs/MyApp/src/App.jsx
@@ -143,56 +143,97 @@ import Card from './Card'
 // For updating variables value on UI, useState is the solution
 // useState always imports from react
 
-function App() {
+// function App() {
 
-  // Syntax of simple variable declaration:
-  // let count = 0;
-  // let firstName = "Lakshaya";
-  // let data = [];
+//   // Syntax of simple variable declaration:
+//   // let count = 0;
+//   // let firstName = "Lakshaya";
+//   // let data = [];
 
-  // Syntax of declaring variable using useState:
-  const [count, setCount] = useState(0);
-  const [firstName, setFirstName] = useState("Lakshaya");
-  const [data, setData] = useState([]);
+//   // Syntax of declaring variable using useState:
+//   const [count, setCount] = useState(0);
+//   const [firstName, setFirstName] = useState("Lakshaya");
+//   const [data, setData] = useState([]);
   
-  console.log(count);
-  console.log(firstName);
-  console.log(data);
+//   console.log(count);
+//   // console.log(firstName);
+//   // console.log(data);
 
 
-  const handleDecCount = function(){
+//   const handleDecCount = function(){
         
-      // Simple value updating:
-        // count = count - 1;
-        // console.log('count : ', count);
+//       // Simple value updating:
+//         // count = count - 1;
+//         // console.log('count : ', count);
 
 
-    // Updating value through useState:
-    setCount(count-1);  // This function is used to set initial value
+//     // Updating value through useState:
+//     setCount(count-1);  // This function is used to set initial value
     
-  }
+//   }
 
-  const handleIncCount = function(){
+//   const handleIncCount = function(){
         
-    // Simple value updating:
-        // count = count + 1;
-        // console.log('count : ', count);
+//     // Simple value updating:
+//         // count = count + 1;
+//         // console.log('count : ', count);
 
 
-    // Updating value through useState:
-    setCount(count+1);  // This function is used to set initial value
+//     // Updating value through useState:
+//     setCount(count+1);  // This function is used to set initial value
 
-  }
+//   }
 
+//   return (
+//     <div>
+//       <button onClick={handleDecCount}>Dec count (-)</button>
+
+//       <p>Count : {count}</p>
+
+//       <button onClick={handleIncCount}>Inc count (+)</button>
+//     </div>
+//   )
+// }
+
+
+
+
+
+
+
+
+// Practice-5:
+
+// Data Transfer:
+// In react you can transfer data from parent component ("Card.jsx") to 
+// child component ("<Card/>") with the help of "props"
+
+// Props: same component contains different data
+// In this example each <Card/> component containes different data
+
+function App() {
+  const firstName = 'Lakshaya';
+  
   return (
     <div>
-      <button onClick={handleDecCount}>Dec count (-)</button>
 
-      <p>Count : {count}</p>
+      <Card image="https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg" title="Dhoom Machale Shoes" price={25000}/> 
 
-      <button onClick={handleIncCount}>Inc count (+)</button>
+      <Card image="https://img.freepik.com/premium-vector/cosmetic-packaging-design_86047-37.jpg?w=740" title="Face Wash" price={500}/>
+
+      <Card image="https://plus.unsplash.com/premium_photo-1678099940967-73fe30680949?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fHByb2R1Y3R8ZW58MHwyfDB8fHww&auto=format&fit=crop&q=60&w=600" title="Next-Gen Headphone" price={1300}/>
+
+      <Card image="https://img.freepik.com/premium-photo/household-cleaning-products_863013-118522.jpg" title="Cleaning Stuff" price={799}/>
+      
+      <Card image="https://images.unsplash.com/photo-1572635196184-84e35138cf62?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHByb2R1Y3R8ZW58MHwyfDB8fHww&auto=format&fit=crop&q=60&w=600" title="Kids Toy Car" price={350}/>
+      
+      <Card image="https://images.unsplash.com/photo-1581191853005-15f8b24f6302?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHByb2R1Y3R8ZW58MHwyfDB8fHww&auto=format&fit=crop&q=60&w=600" title="Coffee Beans" price={1200}/>
+      
+      <Card image="https://images.unsplash.com/photo-1601592908805-7513f10bb3e3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTk1fHxwcm9kdWN0fGVufDB8MnwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=600" title="Ear Buds Ultra" price={1600}/>
     </div>
   )
 }
+
+
 
 export default App
--- 29ReactJs/MyApp/src/Card.jsx
+++ 29ReactJs/MyApp/src/Card.jsx
@@ -1,11 +1,51 @@
-function Card() {
+
+// Card Practice: 
+
+// function Card() {
+//     return (
+//         <div>
+//             <img src="https://imgs.search.brave.com/B_yAIlIHnfzK_wtsVraML1fnbCEJR58zK2K1HZitMuc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wNTAv/NDY5LzUwNi9zbWFs/bC9tb2Rlcm4td2ly/ZWxlc3MtaGVhZHBo/b25lcy13aXRoLWJs/YWNrLWVhcmN1cHMt/YW5kLWEtd2hpdGUt/aGVhZGJhbmQtcmVz/dC1vbi1hLXdoaXRl/LXRhYmxlLXBob3Rv/LmpwZWc" alt="" width="300" />
+//             <h2>Next Gen Headphone</h2>
+//             <p>Rs. 12000</p>
+//         </div>
+//     )
+// }
+
+
+
+
+
+
+
+
+
+
+
+// Props Practice:
+
+// 1. Passing props in components parameter scope
+// 2. Consoling the value of props 
+
+
+function Card(props) {
+
+    // props value console -- it will give you a object of child components data
+    console.log(props);
+
+    // Consoling props objects values
+    console.log(props.image);
+    console.log(props.title);
+    console.log(props.price);
+
     return (
+        // Passing props values -- always provide js value inside component then use -> {}
         <div>
-            <img src="https://imgs.search.brave.com/B_yAIlIHnfzK_wtsVraML1fnbCEJR58zK2K1HZitMuc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wNTAv/NDY5LzUwNi9zbWFs/bC9tb2Rlcm4td2ly/ZWxlc3MtaGVhZHBo/b25lcy13aXRoLWJs/YWNrLWVhcmN1cHMt/YW5kLWEtd2hpdGUt/aGVhZGJhbmQtcmVz/dC1vbi1hLXdoaXRl/LXRhYmxlLXBob3Rv/LmpwZWc" alt="" width="300" />
-            <h2>Next Gen Headphone</h2>
-            <p>Rs. 12000</p>
+            <img src={props.image} alt="" width="250" />
+            <h2>{props.title}</h2>
+            <p>Rs. {props.price}</p>
         </div>
     )
 }
+
 
 export default Card;
