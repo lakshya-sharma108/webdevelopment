cf275048f8783c63426215bca31ac9b042ad7d1e
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2025-12-06 15:19:34 +0530

    Add initial JavaScript practice file with age eligibility check

---
+++ 28lecture_JsStart/101Ultimate-Practice/1practice.js
@@ -0,0 +1,17 @@
+// const data = {
+//     from: 1,
+//     to: 5
+// }
+
+// console.log(...data);
+
+
+
+let age  = 19;
+if (age == 18) {
+    console.log("eligible");
+}
+else{
+    console.log("not eligible");
+    
+}
