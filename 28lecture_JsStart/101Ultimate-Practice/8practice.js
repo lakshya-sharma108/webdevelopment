a26b76ae7686120a270446dc010fb342da91358e
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2025-12-22 15:36:47 +0530

    Add initial HTML and JavaScript practice files for DOM manipulation

---
+++ 28lecture_JsStart/101Ultimate-Practice/8practice.js
@@ -0,0 +1,10 @@
+// Dom:
+
+document.title = "hello ji";
+
+console.log(document.title);
+console.log(document.body);
+
+
+// This will add a inline css:
+document.body.style.backgroundColor = "yellow";
