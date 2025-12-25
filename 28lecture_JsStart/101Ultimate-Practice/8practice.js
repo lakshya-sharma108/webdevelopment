9b66037981bcad63cce364ce09f2fc211f1eac21
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2025-12-25 14:25:23 +0530

    Enhance DOM manipulation examples in 8practice.js with child and sibling node access

--- 28lecture_JsStart/101Ultimate-Practice/8practice.js
+++ 28lecture_JsStart/101Ultimate-Practice/8practice.js
@@ -18,5 +18,78 @@
 
 
 
-
 console.log("Hello world!!");
+
+// Accessing child nodes:
+
+// NOTE: Js includes white spaces as text node.
+
+// this will get me whole ducument body:
+console.log(document.body);
+
+// this will get me all child nodes inside body:
+console.log(document.body.childNodes);
+
+// accessing a particular node using index
+console.log(document.body.childNodes[0]);
+console.log(document.body.childNodes[1]);
+
+
+
+// accessing child node - container div
+console.log(document.body.childNodes[3]);
+
+// accessing child nodes of div container
+console.log(document.body.childNodes[3].ChildNodes);
+
+
+
+
+let cont = document.body.childNodes[3];
+
+// because of white space the first child in div is text
+console.log(cont.firstChild);
+
+// this will ignore space so we will not get text as a child
+// accessing first-last child nodes
+console.log(cont.firstElementChild);
+console.log(cont.lastElementChild);
+
+// applying css on child nodes:
+cont.firstElementChild.style.backgroundColor = "Yellow";
+cont.lastElementChild.style.backgroundColor = "teal";
+
+
+
+// accessing parent node
+console.log(cont.firstElementChild.parentElement);
+
+// applying css on parent child
+cont.firstElementChild.parentElement.style.display = "flex";
+
+
+
+// accessing all child nodes 
+console.log(cont.children);
+// or we can also do this
+console.log(document.body.firstElementChild.children);
+
+
+
+
+// applying css on all child nodes
+// using loop because cont.children returns the array of childs
+// for (const key in cont.children) {
+
+//     cont.children[key].style.color = "black";
+//     cont.children[key].style.backgroundColor = "yellow";
+//     cont.children[key].style.textAlign = "center";
+// }
+
+
+
+// accessing sibling nodes
+console.log(cont.children[3].nextElementSibling);
+console.log(cont.children[3].previousElementSibling);
+
+
