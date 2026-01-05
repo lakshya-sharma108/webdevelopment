b30a88f20a861650256b78e66e52b60d6ca7e939
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-01-05 18:41:17 +0530

    Refactor HTML and JavaScript to enhance layout and event handling functionality

--- 28lecture_JsStart/101Ultimate-Practice/12practice.js
+++ 28lecture_JsStart/101Ultimate-Practice/12practice.js
@@ -1,1 +1,30 @@
 // Events:
+
+let button = document.getElementById("btn");
+let box = document.querySelector(".box");
+
+
+// using Event and EventListner:
+button.addEventListener("click", ()=>{
+    box.innerHTML = "Button mat daba chutiye!!!"
+})
+
+
+// In the above example -- the function "addEventListener" is a function which catch a event of perform some operation
+// It takes two parameter -- 
+// 1. Event -> eg. "click"
+// 2. Arrow function -> it defines what action will execute when event trigrred
+
+
+
+// There are many types of Events:
+// Each type of event contains multiple events:
+
+
+
+
+
+
+
+
+// 
