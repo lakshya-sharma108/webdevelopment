2a25f882ffc7e68be038835f3ee43f90ef414239
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-01-06 13:33:35 +0530

    Refactor event listeners to include stopPropagation and add setTimeout example

--- 28lecture_JsStart/101Ultimate-Practice/12practice.js
+++ 28lecture_JsStart/101Ultimate-Practice/12practice.js
@@ -5,7 +5,7 @@ let box = document.querySelector(".box");
 
 
 // using Event and EventListner:
-button.addEventListener("click", ()=>{
+button.addEventListener("click", () => {
     box.innerHTML = "Button mat daba chutiye!!!"
 })
 
@@ -51,17 +51,54 @@ button.addEventListener("click", ()=>{
 // Solution:  stopPropagation();
 // To stop event bubbling we will take a Event object in arrow function ("e") and inside arrow function we will do -> e.stopPropagation();  this will stop event bubbling
 
-document.querySelector(".container2").addEventListener("click", (e)=>{
+document.querySelector(".container2").addEventListener("click", (e) => {
     e.stopPropagation();
     window.alert("Container2 was clicked!!!");
 })
 
-document.querySelector(".childContainer").addEventListener("click", (e)=>{
+document.querySelector(".childContainer").addEventListener("click", (e) => {
     e.stopPropagation();
     window.alert("childContainer was clicked!!!");
 })
 
-document.querySelector(".child").addEventListener("click", (e)=>{
+document.querySelector(".child").addEventListener("click", (e) => {
     e.stopPropagation();
     window.alert("child box was clicked!!!");
 })
+
+
+
+
+
+
+
+// setInterval() -- used for repetation of a task within a spacific time span
+// It takes two paramenters:  1. arrow function  2. time in miliseconds
+
+// setInterval(() => {
+//     console.log("hello world!!");
+//     console.log("hello ji ");
+    
+// }, 300);
+
+
+// we can use clearInterval() to remove interval
+
+
+
+
+// setTimeout() -- used to pause the execution for once for a spacific time span and after that it will continue 
+// It takes two para:  1. arrow fonction  2. time in milisecond
+
+console.log("hello");
+console.log("hii");
+
+setTimeout(() => {
+    console.log("whats up");    
+}, 1000);
+
+console.log("kya haal");
+console.log("kaise ho");
+
+
+// we can use clearTimeout() to remove Timeout
