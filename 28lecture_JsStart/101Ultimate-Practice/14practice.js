5d1f349b24f2d744b712c4f1651e524344ac42b3
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-01-08 12:27:08 +0530

    Enhance documentation in 14practice.js with detailed explanations of async/await functionality

--- 28lecture_JsStart/101Ultimate-Practice/14practice.js
+++ 28lecture_JsStart/101Ultimate-Practice/14practice.js
@@ -1,2 +1,52 @@
 // Async and Await:
 
+// waht does async and await do?
+
+// Async and awits are used to wait for a task, which takes time in completig
+// with Async and await js waits for a promise to fullfill after that it executes next line
+
+
+// 1. Async: It makes a function return promise automatically
+// 2. Await: It waits for a pormise to fulfill
+
+
+// Why do we need Async?
+// Because await only works inside a async function --
+//  or
+// async allows you to use await inside a function.
+
+
+// And -- await pauses the function until the promise finishes
+
+
+
+// Example:
+
+async function printData() {
+    return new Promise((resolve, reject) => {  
+        setTimeout(() => {
+            resolve("promise is fullfilled!!!")
+        }, 3000);
+    })
+}
+
+
+async function main() {
+    
+    
+    // without async and await js will not wait for time taking task to complete and complete other tasks 
+    // console.log("before function task completed!!!");
+    // printData();
+    // console.log("after function task completed!!!");
+ 
+    
+
+    // but with async and await it will wait to complete the other tasks
+    console.log("before function task completed!!!");
+    let data = await printData();
+    console.log(data);
+    console.log("after function task completed!!!");
+}
+
+
+main();
