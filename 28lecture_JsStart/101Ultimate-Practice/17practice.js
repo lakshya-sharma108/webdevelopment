da92b9f6d14225289e25edae77685309170a205b
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-01-15 14:22:34 +0530

    Add initial HTML and JavaScript files for object-oriented programming practice

---
+++ 28lecture_JsStart/101Ultimate-Practice/17practice.js
@@ -0,0 +1,9 @@
+// JS Object Oriented Programming:
+
+
+// JS object have a special property called prototype, that is either null or references another object
+
+
+
+
+// OPPS concept of js is 97% exact same as JAVA
